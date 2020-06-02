73d0edb856f8edffda4535be0effdfe70c1f7057
BranceCraker <1327968619@qq.com>
2020-06-02 21:51:23 +0800

    add CDateTime

--- Test_05_31_CDatetime/Test_05_31_CDatetime/main.cpp
+++ Test_05_31_CDatetime/Test_05_31_CDatetime/main.cpp
@@ -5,8 +5,9 @@ void main()
 	CDateTime dt;
 	dt.GetCurTime();
 	//cout << dt.GetCurTime()<< endl;
-	//dt.DateTimeShow();
-
+		
+	dt.DateTimeShow();
+				
 	//dt.NextDateTime(100);
 	//dt.PrevTime(100);
 	//dt++;
